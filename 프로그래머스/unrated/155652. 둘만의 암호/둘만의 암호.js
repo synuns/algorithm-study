36c931589aa0e2a530c9e25f9e58c700826787db
Shinwon Jang <57736547+synuns@users.noreply.github.com>
2023-03-13 02:33:58 +0900

    [unrated] Title: 둘만의 암호, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -6,7 +6,6 @@ function solution(s, skip, index) {
         if(skip.indexOf(word) > -1) continue;
         dict.push(word);
     }
-    console.log(dict);
     answer = s.split("").reduce((acc, val, idx) => {
         acc += dict[(dict.indexOf(val)+index) % dict.length];
         return acc;
